e7d9fea7329f611c613204da3f8c86a3e3133582
Polosgoni <161287095+Polosgoni@users.noreply.github.com>
2024-02-27 07:57:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
-global.prems = ['6281395861695']
-global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
+global.owner = ['6281284632647']  
+global.mods = ['6281284632647'] 
+global.prems = ['6281284632647']
+global.nameowner = 'Pai'
+global.numberowner = '6281284632647' 
 global.mail = 'support@tioprm.eu.org' 
 global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
 global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.wm = '© Punya Pai'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -16,11 +16,11 @@ global.autobio = false // Set true untuk mengaktifkan autobio
 global.maxwarn = '2' // Peringatan maksimum
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'KQ7kYKvp' 
 //Daftar terlebih dahulu https://api.botcahx.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.lann = 'YOUR_APIKEY_HERE'
+global.lann = '6Xo4brA5'
 //Daftar https://api.betabotz.eu.org 
 
 global.APIs = {   
